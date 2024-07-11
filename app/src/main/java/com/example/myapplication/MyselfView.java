24ee26c959aa3671107c1e1f301cda8e7235e60e
test <1987901354@qq.com>
2024-07-11 19:07:23 +0800

    把自定义组合控件写完了

--- app/src/main/java/com/example/myapplication/MyselfView.java
+++ app/src/main/java/com/example/myapplication/MyselfView.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MyselfView extends AppCompatActivity {
+import com.example.myapplication.View.TitleBar;
 
-    @Override
+public class MyselfView extends AppCompatActivity {
+    private TitleBar titleBar;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_myself_view);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Intent intent=new Intent(this,AnimatorTest.class);
+        titleBar=findViewById(R.id.titleBar);
+        titleBar.setTitle("这里是自定义组合控件");
+        titleBar.setLeftListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+        titleBar.setRightListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MyselfView.this,"点击右侧的image",Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
--- app/src/main/java/com/example/myapplication/View/TitleBar.java
+++ app/src/main/java/com/example/myapplication/View/TitleBar.java
@@ -1,10 +1,24 @@
 package com.example.myapplication.View;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.myapplication.R;
 
 public class TitleBar extends RelativeLayout {
+    private ImageView titlebar_left,titlebar_right;
+    private TextView titlebar_title;
+    private RelativeLayout titlebar_trootlayout;
+    private int mColor= Color.BLUE;
+    private int mTextColor=Color.WHITE;
+    private String titleName;
     public TitleBar(Context context) {
         super(context);
         initView(context);
@@ -12,6 +26,10 @@ public class TitleBar extends RelativeLayout {
 
     public TitleBar(Context context, AttributeSet attrs) {
         super(context, attrs);
+        TypedArray typedArray=context.obtainStyledAttributes(attrs,R.styleable.TitleBar);
+        mColor=typedArray.getColor(R.styleable.TitleBar_title_bg,Color.BLUE);
+        mTextColor=typedArray.getColor(R.styleable.TitleBar_title_color,Color.WHITE);
+        titleName=typedArray.getString(R.styleable.TitleBar_title_text);
         initView(context);
     }
 
@@ -20,6 +38,24 @@ public class TitleBar extends RelativeLayout {
         initView(context);
     }
     public void initView(Context context){
-
+        LayoutInflater.from(context).inflate(R.layout.my_title,this,true);
+        titlebar_left = findViewById(R.id.titleBar_left);
+        titlebar_title=findViewById(R.id.titleBar_title);
+        titlebar_right=findViewById(R.id.titleBar_right);
+        titlebar_trootlayout=findViewById(R.id.titlebar_rootlayout);
+        titlebar_trootlayout.setBackgroundColor(mColor);
+        titlebar_title.setTextColor(mTextColor);
+        titlebar_title.setText(titleName);
+    }
+    public void setTitle(String titleName){
+        if (!TextUtils.isEmpty(titleName)){
+            titlebar_title.setText(titleName);
+        }
+    }
+    public void setLeftListener(OnClickListener onClickListener){
+        titlebar_left.setOnClickListener(onClickListener);
+    }
+    public void setRightListener(OnClickListener onClickListener){
+        titlebar_right.setOnClickListener(onClickListener);
     }
 }
